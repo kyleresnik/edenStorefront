e445f409930d52b963702dd2c2cb4a7e75f94cea
Kyle Resnik <kylebresnik@gmail.com>
2018-11-27 17:25:28 -0500

    basic stub commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { AdminComponent } from './admin/admin.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HomeComponent,
+    ProductsComponent,
+    AdminComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
